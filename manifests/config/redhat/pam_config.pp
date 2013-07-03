f03a02da9fd955786488e973d66455389f15d756
Jan Vansteenkiste <jan@vstone.eu>
2013-07-03 12:47:58 +0200

    Fix arrow allignment

--- manifests/config/redhat/pam_config.pp
+++ manifests/config/redhat/pam_config.pp
@@ -6,6 +6,7 @@ define ldap_auth::config::redhat::pam_config() {
   }
 
   augeas {"ldap_auth::config-pam-${name}_auth":
+    onlyif  => 'get *[type = "auth"][module = "pam_ldap.so"][argument = "use_first_pass"]/type != auth',
     changes => [
       'ins 01 before *[type = "auth"][module = "pam_deny.so"]',
       'set 01/type auth',
@@ -13,7 +14,6 @@ define ldap_auth::config::redhat::pam_config() {
       'set 01/module pam_ldap.so',
       'set 01/argument use_first_pass',
     ],
-    onlyif => 'get *[type = "auth"][module = "pam_ldap.so"][argument = "use_first_pass"]/type != auth',
   }
 
   augeas {"ldap_auth::config-pam-${name}_account_broken_shadow":
@@ -23,16 +23,17 @@ define ldap_auth::config::redhat::pam_config() {
   }
 
   augeas {"ldap_auth::config-pam-${name}_account":
+    onlyif  => 'get *[type = "account"][control = "[default=bad success=ok user_unknown=ignore]"][module = "pam_ldap.so"]/type != account',
     changes => [
       'ins 01 before *[type = "account"][control = "required"][module = "pam_permit.so"]',
       'set 01/type account',
       'set 01/control "[default=bad success=ok user_unknown=ignore]"',
       'set 01/module pam_ldap.so',
     ],
-    onlyif => 'get *[type = "account"][control = "[default=bad success=ok user_unknown=ignore]"][module = "pam_ldap.so"]/type != account',
   }
 
   augeas {"ldap_auth::config-pam-${name}_password":
+    onlyif  => 'get *[type = "password"][control = "sufficient"][module = "pam_ldap.so"][argument = "use_authtok"]/type != password',
     changes => [
       'ins 01 before *[type = "password"][control = "required"][module = "pam_deny.so"]',
       'set 01/type password',
@@ -40,26 +41,25 @@ define ldap_auth::config::redhat::pam_config() {
       'set 01/module pam_ldap.so',
       'set 01/argument use_authtok',
     ],
-    onlyif => 'get *[type = "password"][control = "sufficient"][module = "pam_ldap.so"][argument = "use_authtok"]/type != password',
   }
 
   augeas {"ldap_auth::config-pam-${name}_session_pam_mkhomedir":
+    onlyif  => 'get *[type = "session"][control = "optional"][module = "pam_mkhomedir.so"]/type != session',
     changes => [
       'ins 01 after *[type = "session"][control = "required"][module = "pam_limits.so"]',
       'set 01/type session',
       'set 01/control optional',
       'set 01/module pam_mkhomedir.so',
     ],
-    onlyif => 'get *[type = "session"][control = "optional"][module = "pam_mkhomedir.so"]/type != session',
   }
 
   augeas {"ldap_auth::config-pam-${name}_session_pam_ldap":
+    onlyif  => 'get *[type = "session"][control = "optional"][module = "pam_ldap.so"]/type != session',
     changes => [
       'ins 01 after *[type = "session"][last()]',
       'set 01/type session',
       'set 01/control optional',
       'set 01/module pam_ldap.so',
     ],
-    onlyif => 'get *[type = "session"][control = "optional"][module = "pam_ldap.so"]/type != session',
   }
 }
