a876f4aa5f64be16b2a136770a67c9217aa58f28
Jan Vansteenkiste <jan@vstone.eu>
2013-07-03 12:45:56 +0200

    Added puppet docs to main class

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,3 +1,46 @@
+# == Class: ldap_auth
+#
+# Set up ldap authentication
+#
+# === Parameters:
+#
+# [*enable*]
+#   Flag indicating we should actually setup ldap authentication.
+#   This can be used in hiera for example to enable/disable ldap authentication
+#   while still including ldap_auth in your 'default' role.
+#
+# [*server*]
+#   Name/IP of the ldap server to use. This is a required parameter!
+#
+# [*base*]
+#   Base DN to use.
+#
+# [*binddn*]
+#   DN to use while binding to the ldap server. Undefined by default.
+#
+# [*bindpw*]
+#   Password to use while binding to the ldap server. Undefined by default.
+#
+# [*ssl*]
+#   Enable ssl while connecting to the ldap server.
+#   This can be either a boolean value which translates in 'on' or 'off' or a
+#   string (start_tls).
+#   Defaults to false.
+#
+# [*filter*]
+#   LDAP search filters to use. This is only used by nslcd.
+#   Defaults to an empty array.
+#
+# === Advanced Parameters:
+#
+# [*packages*]
+#   Override the packages to install for this operatingsystem.
+#   Defaults to OS specific.
+#
+# [*nslcd_service*]
+#   Override the name of the nslcd service.
+#   Defaults to OS specific.
+#
 class ldap_auth (
   $enable         = true,
   $server         = $::ldap_auth::params::server,
